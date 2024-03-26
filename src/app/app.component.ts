7a987568fece36f5eccc8f8f2dd489e0e6102967
Alijaved07 <alijavedofficial0007@gmail.com>
2024-03-26 23:32:06 +0500

    Implemented the structure and logic for creating,clearing and downloading sign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'signmaker';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+  ctx!: CanvasRenderingContext2D | null;
+  isDrawing = false;
+
+  ngAfterViewInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    if (this.ctx) {
+      this.ctx.lineCap = 'round';
+      this.ctx.lineWidth =4;
+    }
+  }
+  
+
+  onMouseMove(event: MouseEvent): void {
+    if (!this.ctx || !this.isDrawing) return;
+
+    const rect = this.canvas.nativeElement.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+  }
+
+  clearCanvas() {
+    this.ctx?.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  }
+
+  downloadSign() {
+    if (this.ctx) {
+      const dataUrl = this.canvas.nativeElement.toDataURL();
+      const a = document.createElement('a');
+      a.href = dataUrl;
+      a.download = 'signature.png';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
 }
